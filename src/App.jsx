178d1f43124c83f8b726d5e403d1f19c79216703
Ishika-45 <bansalishika58@gmail.com>
2025-06-17 21:23:12 +0530

    Charts

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import Navbar from "./scenes/global/Navbar";
 import Sidebar from "./scenes/global/sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/index";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
+import PieChart from "./components/PieChart";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 
 function App() {
@@ -31,14 +32,14 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<PieChart />} />
               {/* <Route path="/line" element={<Line />} /> */}
 {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
 
             </Routes>
           </main>
